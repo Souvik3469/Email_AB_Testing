26035706de864dbea66643aa99acbcf19ea43341
Souvik3469 <souviksen3469@gmail.com>
2024-05-14 20:28:21 +0530

    Experiment controllers and routes

--- backend/src/v1/routes/User.route.js
+++ backend/src/v1/routes/User.route.js
@@ -3,12 +3,15 @@ import {
   userController,
 } from "../controllers";
 import authMiddleware from "../middlewares/Auth.middleware";
-// import tenderController from "../controllers/experiment/experiment";
+import experimentController from "../controllers/experiment/experiment";
 
 
 const router = express.Router();
 
 router.get("/user-details", authMiddleware, userController.userDetails);
-// router.post("/createtender", authMiddleware, tenderController.createTender);
+router.post("/create-exp", authMiddleware, experimentController.createExperiment);
+router.get("/get-exp", authMiddleware, experimentController.getExperiments);
+router.patch("/update-exp", authMiddleware, experimentController.updateExperiment);
+router.delete("/delete-exp", authMiddleware, experimentController.deleteExperiment);
 
 export default router;
